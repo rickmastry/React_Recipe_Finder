1f2cb01e3eb7c14536690f8c33550dd169a732d3
jah rik <jahrik@hotmail.com>
2019-02-26 19:55:38 -0500

    added proxy for api

---
+++ src/components/Recipe.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {Link} from "react-router-dom";
+
+
+const API_KEY = "cc066a48599ff5cd3c3c06699f0cf0c3";
+const proxy = 'https://cors-anywhere.herokuapp.com/';
+
+
+class Recipe extends React.Component {
+    state = {
+        activeRecipe: []
+    }
+    componentDidMount = async () => {
+       
+        const title = this.props.location.state.recipe;
+        const req = await fetch(`${proxy}https://www.food2fork.com/api/search?key=${API_KEY}&q=${title}`);
+
+        const res = await req.json();
+        console.log(res);
+        this.setState({ activeRecipe: res.recipes[0] });
+
+        console.log(this.state.activeRecipe);
+      
+    }
+    render(){
+        const recipe = this.state.activeRecipe;
+        return (
+          <div className="container">
+           { this.state.activeRecipe.length !== 0 &&
+             <div className="active-recipe">
+                <img className="active-recipe__img" src={recipe.image_url} alt={recipe.title}/>
+                <h3 className="active-recipe__title">{recipe.title}</h3>
+                <h4 className="active-recipe__publisher">
+                  Publisher: <span>{recipe.publisher}</span>
+                </h4>
+                <p className="active-recipe__website">
+                 <span><a href={recipe.publisher_url}>{recipe.publisher_url}</a></span>
+                </p>
+                <button className="active-recipe__button">
+
+                 <Link to="/">Go Home</Link>
+                
+                
+                </button>
+  
+              </div>
+             
+           }
+          
+          </div>
+        );
+    }
+};
+
+
+export default Recipe;
