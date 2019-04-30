1fa376a2a39d507d9614e0e88575548f55ff9eb3
jah rik <jahrik@hotmail.com>
2019-04-30 00:15:23 -0400

    Updates

--- precache-manifest.818fa60dbdfe77dd78482bf31a7fa5da.js
+++ precache-manifest.818fa60dbdfe77dd78482bf31a7fa5da.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/React_Recipe_Finder/static/js/runtime~main.3b4ab475.js"
   },
   {
-    "revision": "dfd83ae4acdfba83481b",
-    "url": "/React_Recipe_Finder/static/js/main.dfd83ae4.chunk.js"
+    "revision": "8606be990a01d8c0eae4",
+    "url": "/React_Recipe_Finder/static/js/main.8606be99.chunk.js"
   },
   {
     "revision": "76f6df144c10f3a9f85e",
     "url": "/React_Recipe_Finder/static/js/2.76f6df14.chunk.js"
   },
   {
-    "revision": "dfd83ae4acdfba83481b",
-    "url": "/React_Recipe_Finder/static/css/main.2ae04b2d.chunk.css"
+    "revision": "8606be990a01d8c0eae4",
+    "url": "/React_Recipe_Finder/static/css/main.4c7e8821.chunk.css"
   },
   {
     "revision": "76f6df144c10f3a9f85e",
     "url": "/React_Recipe_Finder/static/css/2.e6d2ac4b.chunk.css"
   },
   {
-    "revision": "cdb6742c5fc6eeac8a06614aa8e67cf1",
+    "revision": "9d72f18d6a8ff8c4d23494dafb501cdb",
     "url": "/React_Recipe_Finder/index.html"
   }
 ];
